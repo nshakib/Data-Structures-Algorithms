5c2885a4730933df38e1b29e74e7eacf09e06880
nshakib <nazmus.shakib.cse@gmail.com>
2026-01-27 12:49:36 +0600

    merge sort method implement

--- sort/mergeSort.js
+++ sort/mergeSort.js
@@ -2,3 +2,36 @@
 //merge sort is a divide and conquer algorithm
 //merge sort is a comparison based algorithm
 
+// merge sort function
+function mergeSort(arr){
+    if(arr.length <= 1) return arr;
+
+    const middle = Math.floor(arr.length/2);
+    const left = arr.slice(0,middle);
+    const right = arr.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+function merge(left, right){
+    const result = [];
+
+    let i =0;
+    let j= 0;
+
+    while(i<left.length && j  < right.length){
+        if(left[i] < right[j]){
+            result.push(left[i])
+            i++;
+        }else{
+            result.push(right[j]);
+            j++;
+        }
+    }
+    result.push(...left.slice(i));
+    result.push(...right.slice(j));
+    return result;
+}
+
+const unsortedArray = [5,4,3,2,1,180,10];
+const sortedArray = mergeSort(unsortedArray);
+console.log(sortedArray);
